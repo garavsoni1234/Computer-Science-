715efc3f31f12ddf86d29f4c9e364a762630ce8e
garavsoni1234 <garavsoni12@gmail.com>
2018-03-14 21:55:21 -0400

    success

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+	
+public void HelloWorld(){
+	System.out.println("Hello World");
+
+}
+
+
+public static void main(String[] args){
+Hello H = new Hello();
+H.HelloWorld();
+
+
+}
+}
